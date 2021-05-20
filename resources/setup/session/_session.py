66b5c5ce66d218867f9ea18b7f8baee864932278
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-05-20 22:09:22 +0300

    Add files via upload

---
+++ resources/setup/session/_session.py
@@ -0,0 +1,22 @@
+# ZED - THON
+
+from telethon.sessions import StringSession as ss
+from telethon.sync import TelegramClient as tc
+
+print("𓆩 SOURCE ZED -  STRING SESSION 𓆪")
+print("")
+
+APP_ID = int(input("⌔∮ ENTER APP ID HERE - "))
+API_HASH = input("⌔∮ ENTER API HASH HERE - ")
+
+with tc(ss(), APP_ID, API_HASH) as client:
+    ics = client.send_message("me", client.session.save())
+    ics.reply("⌔∮ هذا هو كود التيرمكس الخاص بك.\n⌔∮ المطور - @ZEDTHON. ")
+    print("")
+    print("")
+    print(
+        "⌔∮ Below is the STRING_SESSION. You can also find it in your Telegram Saved Messages."
+    )
+    print("")
+    print("")
+    print(client.session.save())
