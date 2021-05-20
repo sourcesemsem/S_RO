13b6f7ccea0d26c41f3ed889074b6b7406619bc4
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-05-20 21:19:08 +0300

    Add files via upload

---
+++ userbot/plugins/tosha/cmds.py
@@ -0,0 +1,30 @@
+"""
+©Zed : @ZedThon
+  - Commends of All Gif
+"""
+
+from . import *
+
+@icssbot.on(icss_cmd(pattern="م20"))
+async def icss(tosh):
+    await eor(tosh, T)
+
+@icssbot.on(icss_cmd(pattern="متحركات تمبلر"))
+async def icss(tosh):
+    await eor(tosh, O)
+
+@icssbot.on(icss_cmd(pattern="متحركات كيوت"))
+async def icss(tosh):
+    await eor(tosh, S)
+
+@icssbot.on(icss_cmd(pattern="متحركات جريئه"))
+async def icss(tosh):
+    await eor(tosh, H)
+
+@icssbot.on(icss_cmd(pattern="متحركات تحشيش"))
+async def icss(tosh):
+    await eor(tosh, A)
+
+
+
+
