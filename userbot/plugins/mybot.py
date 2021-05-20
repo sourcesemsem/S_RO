f6d2ed7b37480f54a4b83e4f0af5e90a123e2d45
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-05-20 21:11:52 +0300

    Add files via upload

---
+++ userbot/plugins/mybot.py
@@ -0,0 +1,12 @@
+# Hey There '
+
+from . import Mb
+from ..tosh import TBOT
+
+@icssbot.on(
+    icss_cmd(
+       pattern="بوتي", outgoing=True
+    )
+)
+async def mybot(k):
+    await eor(k, Mb.format(TBOT))
