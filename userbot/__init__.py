3928b3dcb7c9d1849b4c4d084511f958a441bc5d
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-06-01 21:10:14 +0300

    Add files via upload

---
+++ userbot/__init__.py
@@ -0,0 +1,58 @@
+import time
+
+import heroku3
+
+from .Config import Config
+from .core import logger
+from .core.session import catub
+from resources.Formation.userbot import *
+__version__ = "3.0.0"
+__license__ = "GNU Affero General Public License v3.0"
+__author__ = "ZED_USERBOT <https://github.com/Zedthon/ZED_USERBOT>"
+__copyright__ = "Zedthon Copyright (C) 2020 - 2021  " + __author__
+
+catub.version = __version__
+catub.tgbot.version = __version__
+bot = catub
+
+StartTime = time.time()
+catversion = "3.0.0"
+
+if Config.UPSTREAM_REPO == "goodcat":
+    UPSTREAM_REPO_URL = "https://github.com/Zedthon/ZED_USERBOT"
+elif Config.UPSTREAM_REPO == "badcat":
+    UPSTREAM_REPO_URL = "https://github.com/Jisan09/catuserbot"
+else:
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    BOTLOG = False
+    BOTLOG_CHATID = "me"
+else:
+    BOTLOG = True
+    BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
