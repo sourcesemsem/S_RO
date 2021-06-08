dbefb3bfae8008374b229f4fa1ea47fe18ab3ded
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-06-08 18:45:54 +0300

    Add files via upload

---
+++ userbot/plugins/assistant/__init__.py
@@ -0,0 +1,8 @@
+# for assistant cmd
+from resources.Formation.userbot_utils import *
+
+# for assistant __init__
+from resources.Formation.assistant import *
+
+# For Dev Commend:
+from userbot.tosh import Devt
