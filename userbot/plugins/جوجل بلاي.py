1c553f903735c17069c87d6a54b6e99244502aaf
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-07-10 19:26:57 +0300

    Add files via upload

---
+++ userbot/plugins/جوجل بلاي.py
@@ -0,0 +1,88 @@
+"""Fetch App Details from Playstore.
+.app <app_name> to fetch app details.
+.appr <app_name>  to fetch app details with Xpl0iter request link.
+  ©ZED™ - @ZlZZl77 """
+
+import bs4
+import requests
+
+from userbot import bot
+Name = bot.me.first_name
+
+@icss.on(icss_cmd(pattern="تطبيق (.*)"))
+@icss.on(sudo_cmd(pattern="تطبيق (.*)", allow_sudo=True))
+async def apk(event):
+    app_name = event.pattern_match.group(1)
+    event = await eor(event, "**⌔∮ ججاري البحث عـن التطبيق**")
+    try:
+        remove_space = app_name.split(" ")
+        final_name = "+".join(remove_space)
+        page = requests.get(
+            "https://play.google.com/store/search?q=" + final_name + "&c=apps"
+        )
+        str(page.status_code)
+        soup = bs4.BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
+        results = soup.findAll("div", "ZmHEEd")
+        app_name = (
+            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
+        )
+        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
+        app_dev_link = (
+            "https://play.google.com"
+            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
+        )
+        app_rating = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "pf5lIe")
+            .find("div")["aria-label"]
+        )
+        app_link = (
+            "https://play.google.com"
+            + results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "vU6FJ p63iDd")
+            .a["href"]
+        )
+        app_icon = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "uzcko")
+            .img["data-src"]
+        )
+        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
+        app_details = "<b>𓆰 " + app_name + " 𓆪</b>"
+        app_details += (
+            "\n\n<b>⌔∮ المطور :</b> <a href='"
+            + app_dev_link
+            + "'>"
+            + app_dev
+            + "</a>"
+        )
+        app_details += "\n<b>⌔∮ تقييم التطبيق :</b> " + app_rating.replace(
+            "صـنفت ", "⭐ "
+        ).replace(" out of ", "/").replace(" الـنجوم", "", 1).replace(
+            " الـنجوم", "⭐ "
+        ).replace(
+            "خـمس", "5"
+        )
+        app_details += (
+            "\n<b>⌔∮ الـمميزات :</b> <a href='"
+            + app_link
+            + "'>لتحميلها من سوق بلي</a>"
+        )
+        app_details += f"\n\n    𓍹 {Name} 𓍻"
+        await event.edit(app_details, link_preview=True, parse_mode="HTML")
+    except IndexError:
+        await event.edit("** لم يتم العثور على نتائج البحث يرجى وضع اسم تطبيق متوفر ❕**")
+    except Exception as err:
+        await event.edit("Exception Occured:- " + str(err))
+
+CMD_HELP.update(
+    {
+        "جوجل بلاي": "**Plugin :** `جوجل بلاي`\
+        \n**Syntax : **`.تطبيق [اسم التطبيق]`\
+        \n**Usage: **searches the app in the playstore and provides the link to the app in playstore and fetchs app details \
+        "
+    }
+)
---
+++ userbot/plugins/يوتيوب.py
@@ -0,0 +1,249 @@
+#ZedThon
+
+import asyncio
+import os
+import re
+import time
+from datetime import datetime
+from pathlib import Path
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.types import DocumentAttributeAudio
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (
+    ContentTooShortError,
+    DownloadError,
+    ExtractorError,
+    GeoRestrictedError,
+    MaxDownloadsReached,
+    PostProcessingError,
+    UnavailableVideoError,
+    XAttrMetadataError,
+)
+
+from . import hmention, progress, ytsearch
+
+
+@icssbot.on(admin_cmd(pattern="تحميل(ص|ف)(?: |$)(.*)", outgoing=True))
+@icssbot.on(sudo_cmd(pattern="تحميل(ص|ف)(?: |$)(.*)", allow_sudo=True))
+async def download_video(v_url):
+    url = v_url.pattern_match.group(2)
+    if not url:
+        rmsg = await v_url.get_reply_message()
+        myString = rmsg.text
+        url = re.search("(?P<url>https?://[^\s]+)", myString).group("url")
+    if not url:
+        await edit_or_reply(v_url, "**عـليك ادراج رابـط مع الامر اولا ليتـم التحميـل**")
+        return
+    ytype = v_url.pattern_match.group(1).lower()
+    v_url = await edit_or_reply(v_url, "**جـارِ التحميل انتظر قليلا**")
+    reply_to_id = await reply_id(v_url)
+    if ytype == "ص":
+        opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+            "outtmpl": "%(id)s.mp3",
+            "quiet": True,
+            "logtostderr": False,
+        }
+        video = False
+        song = True
+    elif ytype == "v":
+        opts = {
+            "format": "best",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
+            ],
+            "outtmpl": "%(id)s.mp4",
+            "logtostderr": False,
+            "quiet": True,
+        }
+        song = False
+        video = True
+    try:
+        await v_url.edit("**- يتم جلب البيانات انتظر قليلا**")
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url)
+    except DownloadError as DE:
+        await v_url.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await v_url.edit("**- عذرا هذا المحتوى قصير جدا لتنزيله ⚠️**")
+        return
+    except GeoRestrictedError:
+        await v_url.edit(
+            "**- الفيديو غير متاح من موقعك الجغرافي بسبب القيود الجغرافية التي يفرضها موقع الويب ❕**"
+        )
+        return
+    except MaxDownloadsReached:
+        await v_url.edit("**- تم الوصول إلى الحد الأقصى لعدد التنزيلات ❕**")
+        return
+    except PostProcessingError:
+        await v_url.edit("**كان هناك خطأ أثناء المعالجة**")
+        return
+    except UnavailableVideoError:
+        await v_url.edit("`الوسائط غير متوفرة بالتنسيق المطلوب`")
+        return
+    except XAttrMetadataError as XAME:
+        await v_url.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await v_url.edit("**حدث خطأ أثناء استخراج المعلومات يرجى وضعها بشكل صحيح ⚠️**")
+        return
+    except Exception as e:
+        await v_url.edit(f"{str(type(e)): {str(e)}}")
+        return
+    itime = time.time()
+    icstb = Path(f"{ytdl_data['id']}.jpg")
+    if not os.path.exists(icstb):
+        icstb = Path(f"{ytdl_data['id']}.webp")
+    if not os.path.exists(icstb):
+        icstb = None
+    if song:
+        await v_url.edit(
+            f"**التحضيـر للـرفع انتظر**:\
+            \n**{ytdl_data['title']}**\
+            \nبـواسطة *{ytdl_data['uploader']}*"
+        )
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{ytdl_data['id']}.mp3",
+            supports_streaming=True,
+            thumb=icatb,
+            reply_to=reply_to_id,
+            attributes=[
+                DocumentAttributeAudio(
+                    duration=int(ytdl_data["duration"]),
+                    title=str(ytdl_data["title"]),
+                    performer=str(ytdl_data["uploader"]),
+                )
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, v_url, itime, "Uploading..", f"{ytdl_data['title']}.mp3")
+            ),
+        )
+        os.remove(f"{ytdl_data['id']}.mp3")
+    elif video:
+        await v_url.edit(
+            f"`Preparing to upload video:`\
+        \n**{ytdl_data['title']}**\
+        \nby *{ytdl_data['uploader']}*"
+        )
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{ytdl_data['id']}.mp4",
+            reply_to=reply_to_id,
+            supports_streaming=True,
+            caption=ytdl_data["title"],
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, v_url, itime, "Uploading..", f"{ytdl_data['title']}.mp4")
+            ),
+        )
+        os.remove(f"{ytdl_data['id']}.mp4")
+    if icstb:
+        os.remove(icstb)
+    await v_url.delete()
+
+
+@icssbot.on(admin_cmd(pattern="يوتيوب(?: |$)(\d*)? ?(.*)", command="يوتيوب"))
+@icssbot.on(sudo_cmd(pattern="يوتيوب(?: |$)(\d*)? ?(.*)", command="يوتيوب", allow_sudo=True))
+async def yt_search(event):
+    if event.fwd_from:
+        return
+    if event.is_reply and not event.pattern_match.group(2):
+        query = await event.get_reply_message()
+        query = str(query.message)
+    else:
+        query = str(event.pattern_match.group(2))
+    if not query:
+        return await edit_delete(
+            event, "**بالرد على كلمه للبحث عنها او بوضع الكلمه مع الامر**"
+        )
+    video_q = await edit_or_reply(event, "**جـارِ البحث...**")
+    if event.pattern_match.group(1) != "":
+        lim = int(event.pattern_match.group(1))
+        if lim <= 0:
+            lim = int(10)
+    else:
+        lim = int(10)
+    try:
+        full_response = await ytsearch(query, limit=lim)
+    except Exception as e:
+        return await edit_delete(video_q, str(e), time=10, parse_mode=parse_pre)
+    reply_text = f"**•  اليك عزيزي قائمة بروابط الكلمة اللتي بحثت عنها:**\n`{query}`\n\n**•  النتائج:**\n{full_response}"
+    await edit_or_reply(video_q, reply_text)
+
+
+@icssbot.on(admin_cmd(pattern="انستا (.*)"))
+@icssbot.on(sudo_cmd(pattern="انستا (.*)", allow_sudo=True))
+async def kakashi(event):
+    if event.fwd_from:
+        return
+    chat = "@instasavegrambot"
+    link = event.pattern_match.group(1)
+    if "www.instagram.com" not in link:
+        await edit_or_reply(
+            event, "` احتاج لرابط الانستا للتنزيل هذا المقطع...`(*_*)"
+        )
+    else:
+        start = datetime.now()
+        icse = await edit_or_reply(event, "**جاري التحميل.....**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            msg = await conv.send_message(link)
+            video = await conv.get_response()
+            details = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await icse.edit("**Error:** `unblock` @instasavegrambot `and retry!`")
+            return
+        await icse.delete()
+        ics = await event.client.send_file(
+            event.chat_id,
+            video,
+        )
+        end = datetime.now()
+        ms = (end - start).seconds
+        await cat.edit(
+            f"<b><i>➥ Video uploaded in {ms} seconds.</i></b>\n<b><i>➥ Uploaded by :- {hmention}</i></b>",
+            parse_mode="html",
+        )
+    await event.client.delete_messages(
+        conv.chat_id, [msg_start.id, response.id, msg.id, video.id, details.id]
+    )
+
+
+CMD_HELP.update(
+    {
+        "يوتيوب": "**Plugin :** `يوتيوب`\
+    \n\n  •  **Syntax :** `.تحميل ص + الرابط`\
+    \n  •  **Function : **__downloads the audio from the given link(Suports the all sites which support youtube-dl)__\
+    \n\n  •  **Syntax : **`.تحميل ف + الرابط`\
+    \n  •  **Function : **__downloads the video from the given link(Suports the all sites which support youtube-dl)__\
+    \n\n  •  **Syntax : **`.يوتيوب query`/`.يوتيوب count query`\
+    \n  •  **Function : **__Fetches youtube search results with views and duration with required no of count results by default it fetches 10 results__\
+    \n\n  •  **Syntax : **`.انستا` <link>\
+    \n  •  **Function : **__Downloads the video from the given instagram link__\
+    "
+    }
+)
