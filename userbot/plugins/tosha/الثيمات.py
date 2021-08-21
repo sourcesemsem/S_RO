853f0c4fca1b2a2be0e3f053777e4d83f91a8bdc
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-08-22 00:09:13 +0300

    Add files via upload

---
+++ userbot/plugins/tosha/الثيمات.py
@@ -0,0 +1,164 @@
+#حقوق زد ثـون ومتعوب عليها .. تخمط اذكر المصدر حبي
+
+"""
+©Zed : @ZedThon
+  - Tumblr Gif -1
+  - Tumblr Gif -2
+  - Tumblr Gif -3
+  - Tumblr Gif -4
+  - Tumblr Gif -5
+  - Tumblr Gif -6
+  - Tumblr Gif -7
+
+"""
+
+
+from .. import reply_id as rd 
+from . import *
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث1$"))
+@bot.on(sudo_cmd(pattern="ث1$", allow_sudo=True))
+async def stsTHMAT(kim):
+    if kim.fwd_from:
+        return
+    zel = await rd(kim)
+    if sts_attheme:
+        kim_c = f"**{THMAT}**\n"
+        await kim.client.send_file(kim.chat_id, sts_attheme, caption=kim_c, reply_to=zel)
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث2$"))
+@bot.on(sudo_cmd(pattern="ث2$", allow_sudo=True))
+async def stsTHMAT(lon):
+    if lon.fwd_from:
+        return
+    lonid = await rd(lon)
+    if sts_attheme2:
+        ics_c = f"**{THMAT}**\n"
+        await lon.client.send_file(lon.chat_id, sts_attheme2, caption=ics_c, reply_to=lonid)
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث3$"))
+@bot.on(sudo_cmd(pattern="ث3$", allow_sudo=True))
+async def stsTHMAT(i):
+    if i.fwd_from:
+        return
+    sic_id = await rd(i)
+    if sts_attheme3:
+        tumc = f"**{THMAT}**\n"
+        await i.client.send_file(i.chat_id, sts_attheme3, caption=tumc, reply_to=sic_id)
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث4$"))
+@bot.on(sudo_cmd(pattern="ث4$", allow_sudo=True))
+async def stsTHMAT(lon):
+    if lon.fwd_from:
+        return
+    reply_to_id = await rd(lon)
+    if sts_attheme4:
+        tumc = f"**{THMAT}**\n"
+        await lon.client.send_file(
+            lon.chat_id, sts_attheme4, caption=tumc, reply_to=reply_to_id
+        )
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث5$"))
+@bot.on(sudo_cmd(pattern="ث5$", allow_sudo=True))
+
+async def stsTHMAT(tosh):
+    if tosh.fwd_from:
+        return
+    reply_to_id = await rd(tosh)
+    if sts_attheme5:
+        tumc = f"**{THMAT}**\n"
+        await tosh.client.send_file(
+            tosh.chat_id, sts_attheme5, caption=tumc, reply_to=reply_to_id
+        )
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث6$"))
+@bot.on(sudo_cmd(pattern="ث6$", allow_sudo=True))
+
+async def stsTHMAT(kimo):
+    if kimo.fwd_from:
+        return
+    reply_to_id = await rd(kimo)
+    if sts_attheme6:
+        tumc = f"**{THMAT}**\n"
+        await kimo.client.send_file(
+            kimo.chat_id, sts_attheme6, caption=tumc, reply_to=reply_to_id
+        )
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث7$"))
+@bot.on(sudo_cmd(pattern="ث7$", allow_sudo=True))
+async def stsTHMAT(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_attheme7:
+        tumc = f"**{THMAT}**\n"
+        await ics.client.send_file(
+            ics.chat_id, sts_attheme7, caption=tumc, reply_to=reply_to_id
+        )
+        
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ث8$"))
+@bot.on(sudo_cmd(pattern="ث8$", allow_sudo=True))
+async def stsTHMAT(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_attheme8:
+        tumc = f"**{THMAT}**\n"
+        await ics.client.send_file(
+            ics.chat_id, sts_attheme8, caption=tumc, reply_to=reply_to_id
+        )
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ث9$"))
+@bot.on(sudo_cmd(pattern="ث9$", allow_sudo=True))
+async def stsTHMAT(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_attheme9:
+        tumc = f"**{THMAT}**\n"
+        await ics.client.send_file(
+            ics.chat_id, sts_attheme9, caption=tumc, reply_to=reply_to_id
+        )
+
+@bot.on(icss_cmd(outgoing=True, pattern="ث10$"))
+@bot.on(sudo_cmd(pattern="ث10$", allow_sudo=True))
+async def stsTHMAT(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_attheme10:
+        tumc = f"**{THMAT}**\n"
+        await ics.client.send_file(
+            ics.chat_id, sts_attheme10, caption=tumc, reply_to=reply_to_id
+        )
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ث11$"))
+@bot.on(sudo_cmd(pattern="ث11$", allow_sudo=True))
+async def stsTHMAT(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_attheme11:
+        tumc = f"**{THMAT}**\n"
+        await ics.client.send_file(
+            ics.chat_id, sts_attheme11, caption=tumc, reply_to=reply_to_id
+        )
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ث12$"))
+@bot.on(sudo_cmd(pattern="ث12$", allow_sudo=True))
+async def stsTHMAT(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_attheme12:
+        tumc = f"**{THMAT}**\n"
+        await ics.client.send_file(
+            ics.chat_id, sts_attheme12, caption=tumc, reply_to=reply_to_id
+        )
