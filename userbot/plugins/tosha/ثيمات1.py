6069dd7da084b5578392db5899b5ff87308012d9
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-08-22 18:45:25 +0300

    Add files via upload

---
+++ userbot/plugins/tosha/ثيمات1.py
@@ -0,0 +1,200 @@
+#حقوق زد ثـون ومتعوب عليها .. تخمط اذكر المصدر حبي
+
+"""
+©Zed : @ZedThon
+  - Tumblr Gif -1
+  - Tumblr Gif -2
+  - Tumblr Gif -3
+  - Tumblr Gif -4
+  - Tumblr Gif -5
+  - Tumblr Gif -6
+  - Tumblr Gif -7
+
+"""
+
+
+from .. import reply_id as rd 
+from . import *
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن1$"))
+@bot.on(sudo_cmd(pattern="ن1$", allow_sudo=True))
+async def stsfanan(kim):
+    if kim.fwd_from:
+        return
+    zel = await rd(kim)
+    if sts_fanan:
+        kim_c = f"**{FANAN}**\n"
+        kim_c += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        kim_c += f"**⪼ ثيم علم العراق 🇮🇶♥️𓆰 أللهم أحفظ العراق وأهله وجميع المسلمين يارب**\n"
+        kim_c += f"**⪼ لتحميل الثيم ارسـل ↫ .ث1**"
+        await kim.client.send_file(kim.chat_id, sts_fanan, caption=kim_c, reply_to=zel)
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن2$"))
+@bot.on(sudo_cmd(pattern="ن2$", allow_sudo=True))
+async def stsfanan(lon):
+    if lon.fwd_from:
+        return
+    lonid = await rd(lon)
+    if sts_fanan2:
+        ics_c = f"**{FANAN}**\n"
+        ics_c += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        ics_c += f"**⪼ ثيم البشير شو HD غير مضر للعيون ❤️ 𓆰...**\n"
+        ics_c += f"**⪼ لتحميل الثيم ارسـل ↫ .ث2**"
+        await lon.client.send_file(lon.chat_id, sts_fanan2, caption=ics_c, reply_to=lonid)
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن3$"))
+@bot.on(sudo_cmd(pattern="ن3$", allow_sudo=True))
+async def stsfanan(i):
+    if i.fwd_from:
+        return
+    sic_id = await rd(i)
+    if sts_fanan3:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم البشير_شو2..ثيم تجريبي🧸❤️ 𓆰**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث3**"
+        await i.client.send_file(i.chat_id, sts_fanan3, caption=tumc, reply_to=sic_id)
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن4$"))
+@bot.on(sudo_cmd(pattern="ن4$", allow_sudo=True))
+async def stsfanan(lon):
+    if lon.fwd_from:
+        return
+    reply_to_id = await rd(lon)
+    if sts_fanan4:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم احمر وازرق بخلفية جوكر بنت كارتونيه متدرج بأحترافيه🧸🧡𓆰**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث4**"
+        await lon.client.send_file(
+            lon.chat_id, sts_fanan4, caption=tumc, reply_to=reply_to_id
+        )
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن5$"))
+@bot.on(sudo_cmd(pattern="ن5$", allow_sudo=True))
+
+async def stsfanan(tosh):
+    if tosh.fwd_from:
+        return
+    reply_to_id = await rd(tosh)
+    if sts_fanan5:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم بخلفية بناتية بألوان متنوعة ومتدرجه ولماعة جداً غير مضر للعيون🎁🤍**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث5**"
+        await tosh.client.send_file(
+            tosh.chat_id, sts_fanan5, caption=tumc, reply_to=reply_to_id
+        )
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن6$"))
+@bot.on(sudo_cmd(pattern="ن6$", allow_sudo=True))
+
+async def stsfanan(kimo):
+    if kimo.fwd_from:
+        return
+    reply_to_id = await rd(kimo)
+    if sts_fanan6:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم للفنانة (بيلي اليش) الثيم ازرق فاتح وأبيض غير مضر للعيون🧸💙𓆰**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث6**"
+        await kimo.client.send_file(
+            kimo.chat_id, sts_fanan6, caption=tumc, reply_to=reply_to_id
+        )
+
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن7$"))
+@bot.on(sudo_cmd(pattern="ن7$", allow_sudo=True))
+async def stsfanan(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_fanan7:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم اخضر وأبيض للفنانه بيلي اليش🧸💚𓆰...**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث7**"
+        await ics.client.send_file(
+            ics.chat_id, sts_fanan7, caption=tumc, reply_to=reply_to_id
+        )
+        
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ن8$"))
+@bot.on(sudo_cmd(pattern="ن8$", allow_sudo=True))
+async def stsfanan(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_fanan8:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم فرقه ( BTS ) الثيم متناسق مع قليل من الشفافيه 🧸🌌𓆰...**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث8**"
+        await ics.client.send_file(
+            ics.chat_id, sts_fanan8, caption=tumc, reply_to=reply_to_id
+        )
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ن9$"))
+@bot.on(sudo_cmd(pattern="ن9$", allow_sudo=True))
+async def stsfanan(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_fanan9:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم لفرقة bts الكورية باللون الأسود🧸🌌𓆰..**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث9**"
+        await ics.client.send_file(
+            ics.chat_id, sts_fanan9, caption=tumc, reply_to=reply_to_id
+        )
+
+@bot.on(icss_cmd(outgoing=True, pattern="ن10$"))
+@bot.on(sudo_cmd(pattern="ن10$", allow_sudo=True))
+async def stsfanan(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_fanan10:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم لفرقة bts الكورية بلون فاتح غير مضر للعيون 🧸🌁𓆰...**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث10**"
+        await ics.client.send_file(
+            ics.chat_id, sts_fanan10, caption=tumc, reply_to=reply_to_id
+        )
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ن11$"))
+@bot.on(sudo_cmd(pattern="ن11$", allow_sudo=True))
+async def stsfanan(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_fanan11:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم أنمي برسائل شفافه غير مضر للعيون 🧸🤍𓆰...**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث11**"
+        await ics.client.send_file(
+            ics.chat_id, sts_fanan11, caption=tumc, reply_to=reply_to_id
+        )
+        
+@bot.on(icss_cmd(outgoing=True, pattern="ن12$"))
+@bot.on(sudo_cmd(pattern="ن12$", allow_sudo=True))
+async def stsfanan(ics):
+    if ics.fwd_from:
+        return
+    reply_to_id = await rd(ics)
+    if sts_fanan12:
+        tumc = f"**{FANAN}**\n"
+        tumc += f"•━─━─━─━─𝙕𝞝𝘿─━─━─━─━•\n\n"
+        tumc += f"**⪼ ثيم داكن أزرق غامق وأبيض خفيف، بقليل من الشفافية🤍𓆰...**\n"
+        tumc += f"**⪼ لتحميل الثيم ارسـل ↫ .ث12**"
+        await ics.client.send_file(
+            ics.chat_id, sts_fanan12, caption=tumc, reply_to=reply_to_id
+        )
