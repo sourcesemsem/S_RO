5408499e87b630f85dc3eee5015a14c13b69ae09
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-08-22 19:15:47 +0300

    Add files via upload

---
+++ userbot/plugins/tosha/ثيمات.py
@@ -0,0 +1,18 @@
+"""
+©Zed : @ZedThon
+  - Commends of All Theme
+"""
+
+from . import *
+
+@bot.on(admin_cmd(pattern="م30"))
+@bot.on(sudo_cmd(pattern="م30", allow_sudo=True))
+async def icss(zelzal):
+    await eor(zelzal, X)
+
+@bot.on(admin_cmd(pattern="ثيمات"))
+@bot.on(sudo_cmd(pattern="ثيمات", allow_sudo=True))
+async def icss(zelzal):
+    await eor(zelzal, ZL)
+
+
