1816bd11f4e718b20b58e55ad06e3c1f73d43ec6
Zedthon <84479740+Zedthon@users.noreply.github.com>
2021-10-03 19:11:57 +0300

    Delete extdl.py

--- userbot/helpers/utils/extdl.py
+++
@@ -1,8 +0,0 @@
-from subprocess import PIPE, Popen
-
-
-def install_pip(pipfile):
-    pip_cmd = ["pip", "install", f"{pipfile}"]
-    process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    return stdout
