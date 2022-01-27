a30fefe5da8bc98b21b54faf010c7c05175395f2
Zedthon <84479740+Zedthon@users.noreply.github.com>
2022-01-27 20:30:54 +0300

    Create ثيمات.py

--- userbot/plugins/tosha/ثيمات.py
+++ userbot/plugins/tosha/ثيمات.py
@@ -7,12 +7,12 @@ from . import *
 
 @bot.on(admin_cmd(pattern="م30"))
 @bot.on(sudo_cmd(pattern="م30", allow_sudo=True))
-async def icss(zelzal):
+async def zed(zelzal):
     await eor(zelzal, X)
 
 @bot.on(admin_cmd(pattern="ثيمات"))
 @bot.on(sudo_cmd(pattern="ثيمات", allow_sudo=True))
-async def icss(zelzal):
+async def zed(zelzal):
     await eor(zelzal, ZL)
 
 
