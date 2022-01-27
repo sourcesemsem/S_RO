50290a659a956da14cf8c9160a4e25079211abb2
Zedthon <84479740+Zedthon@users.noreply.github.com>
2022-01-27 18:57:56 +0300

    Create ريبو.py

--- userbot/plugins/ريبو.py
+++ userbot/plugins/ريبو.py
@@ -5,7 +5,7 @@ O = Config.OWNER_ID
 Name = bot.me.first_name
 M = f"[{Name}](tg://user?id={O})"
 
-A = "https://dashboard.heroku.com/new?template=https://github.com/Zedthon/zedpack/"
+A = "https://t.me/ZedThon/105"
 
 B = "**⌔∮ اهلا عزيزي - {} \n⌔∮ رابط التنصيب - [اضغط هنا]({})**"
 
