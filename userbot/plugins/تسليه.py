8a9185efdd39734943a4db9e6b3b2699c7feeaa7
Zedthon <84479740+Zedthon@users.noreply.github.com>
2022-01-27 17:40:25 +0300

    Create تسليه.py

--- userbot/plugins/تسليه.py
+++ userbot/plugins/تسليه.py
@@ -5,8 +5,8 @@ from . import ALIVE_NAME
 DEF = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
 
-@icssbot.on(admin_cmd(pattern="ميت (.*)"))
-@icssbot.on(sudo_cmd(pattern="ميت (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="ميت (.*)"))
+@bot.on(sudo_cmd(pattern="ميت (.*)", allow_sudo=True))
 async def kakashi(ded):
     name = ded.pattern_match.group(1)
     await edit_or_reply(
@@ -28,8 +28,8 @@ async def kakashi(ded):
     )
 
 
-@icssbot.on(admin_cmd(pattern="مافيا (.*)"))
-@icssbot.on(sudo_cmd(pattern="مافيا (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="مافيا (.*)"))
+@bot.on(sudo_cmd(pattern="مافيا (.*)", allow_sudo=True))
 async def kakashi(killer):
     name = killer.pattern_match.group(1)
     await edit_or_reply(
@@ -325,80 +325,80 @@ async def bluedevilmonster(monster):
     await edit_or_reply(monster, A)
 
 
-@icssbot.on(admin_cmd(pattern=r"خنزير$"))
-@icssbot.on(sudo_cmd(pattern="خنزير$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"خنزير$"))
+@bot.on(sudo_cmd(pattern="خنزير$", allow_sudo=True))
 async def bluedevilpig(pig):
     await edit_or_reply(pig, B)
 
 
-@icssbot.on(admin_cmd(pattern=r"مسدس$"))
-@icssbot.on(sudo_cmd(pattern="مسدس$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"مسدس$"))
+@bot.on(sudo_cmd(pattern="مسدس$", allow_sudo=True))
 async def bluedevilgun(gun):
     await edit_or_reply(gun, D)
 
 
-@icssbot.on(admin_cmd(pattern=r"كلب$"))
-@icssbot.on(sudo_cmd(pattern="كلب$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"كلب$"))
+@bot.on(sudo_cmd(pattern="كلب$", allow_sudo=True))
 async def bluedevildog(dog):
     await edit_or_reply(dog, E)
 
 
-@icssbot.on(admin_cmd(pattern=r"هلو$"))
-@icssbot.on(sudo_cmd(pattern="هلو$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"هلو$"))
+@bot.on(sudo_cmd(pattern="هلو$", allow_sudo=True))
 async def bluedevilhello(hello):
     await edit_or_reply(hello, F)
 
 
-@icssbot.on(admin_cmd(pattern=r"طائره$"))
-@icssbot.on(sudo_cmd(pattern="طائره$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"طائره$"))
+@bot.on(sudo_cmd(pattern="طائره$", allow_sudo=True))
 async def bluedevilhmf(hmf):
     await edit_or_reply(hmf, G)
 
 
-@icssbot.on(admin_cmd(pattern=r"حبايب$"))
-@icssbot.on(sudo_cmd(pattern="حبايب$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"حبايب$"))
+@bot.on(sudo_cmd(pattern="حبايب$", allow_sudo=True))
 async def bluedevilcouple(couple):
     await edit_or_reply(couple, H)
 
 
-@icssbot.on(admin_cmd(pattern=r"سوبر$"))
-@icssbot.on(sudo_cmd(pattern="سوبر$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"سوبر$"))
+@bot.on(sudo_cmd(pattern="سوبر$", allow_sudo=True))
 async def bluedevilsupreme(supreme):
     await edit_or_reply(supreme, I)
 
 
-@icssbot.on(admin_cmd(pattern=r"الهند$"))
-@icssbot.on(sudo_cmd(pattern="الهند$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"الهند$"))
+@bot.on(sudo_cmd(pattern="الهند$", allow_sudo=True))
 async def bluedevilindia(india):
     await edit_or_reply(india, J)
 
 
-@icssbot.on(admin_cmd(pattern=r"هلا$"))
-@icssbot.on(sudo_cmd(pattern="هلا$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"هلا$"))
+@bot.on(sudo_cmd(pattern="هلا$", allow_sudo=True))
 async def bluedevilwelcome(welcome):
     await edit_or_reply(welcome, K)
 
 
-@icssbot.on(admin_cmd(pattern=r"حيه$"))
-@icssbot.on(sudo_cmd(pattern="حيه$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"حيه$"))
+@bot.on(sudo_cmd(pattern="حيه$", allow_sudo=True))
 async def bluedevilsnake(snake):
     await edit_or_reply(snake, L)
 
 
-@icssbot.on(admin_cmd(pattern=r"باي$"))
-@icssbot.on(sudo_cmd(pattern="باي$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"باي$"))
+@bot.on(sudo_cmd(pattern="باي$", allow_sudo=True))
 async def bluedevilbye(bye):
     await edit_or_reply(bye, O)
 
 
-@icssbot.on(admin_cmd(pattern=r"خرا$"))
-@icssbot.on(sudo_cmd(pattern="خرا$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"خرا$"))
+@bot.on(sudo_cmd(pattern="خرا$", allow_sudo=True))
 async def bluedevilshitos(shitos):
     await edit_or_reply(shitos, P)
 
 
-@icssbot.on(admin_cmd(pattern=r"دس لايك$"))
-@icssbot.on(sudo_cmd(pattern="دس لايك$", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"دس لايك$"))
+@bot.on(sudo_cmd(pattern="دس لايك$", allow_sudo=True))
 async def bluedevildislike(dislike):
     await edit_or_reply(dislike, R)
 
