88c37b50bc0e8fa636af24773538723539c769a4
Zedthon <84479740+Zedthon@users.noreply.github.com>
2022-01-27 19:02:57 +0300

    Update رد مؤقت.py

--- userbot/plugins/رد مؤقت.py
+++ userbot/plugins/رد مؤقت.py
@@ -3,14 +3,14 @@
 from asyncio import sleep
 
 
-@icssbot.on(admin_cmd(pattern="رد (\d*) (.*)", outgoing=True))
-@icssbot.on(sudo_cmd(pattern="رد (\d*) (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="رد (\d*) (.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="رد (\d*) (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    ics = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
-    message = ics[1]
-    ttl = int(ics[0])
+    zel = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    message = zel[1]
+    ttl = int(zel[0])
     try:
         await event.delete()
     except Exception as e:
