6a4a71664ef69667ce613d9c505967be5490d3c5
Zedthon <84479740+Zedthon@users.noreply.github.com>
2022-01-27 18:03:27 +0300

    Create متحركات2.py

--- userbot/plugins/tosha/متحركات2.py
+++ userbot/plugins/tosha/متحركات2.py
@@ -4,103 +4,103 @@ from .. import reply_id as rd
 from . import *
 
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش1$"))
-@icssbot.on(sudo_cmd(pattern="ش1$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش1$"))
+@bot.on(sudo_cmd(pattern="ش1$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    Ti = await rd(icss)
+    Ti = await rd(zed)
     if fy_gif:
-        icss_caption = f"**{TSHE}**\n"
-        icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        icss_caption += f"**↫ المتـحركه الاولى 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif, caption=icss_caption, reply_to=Ti
+        zed_caption = f"**{TSHE}**\n"
+        zed_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        zed_caption += f"**↫ المتـحركه الاولى 𓆰.**"
+        await zed.client.send_file(
+            zed.chat_id, fy_gif, caption=zed_caption, reply_to=Ti
         )
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش2$"))
-@icssbot.on(sudo_cmd(pattern="ش2$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش2$"))
+@bot.on(sudo_cmd(pattern="ش2$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    th = await rd(icss)
+    th = await rd(zed)
     if fy_gif2:
-        icss_caption = f"**{TSHE}**\n"
-        icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        icss_caption += f"**↫ المتـحركه الثانيه 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif2, caption=icss_caption, reply_to=th
+        zed_caption = f"**{TSHE}**\n"
+        zed_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        zed_caption += f"**↫ المتـحركه الثانيه 𓆰.**"
+        await zed.client.send_file(
+            zed.chat_id, fy_gif2, caption=zed_caption, reply_to=th
         )
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش3$"))
-@icssbot.on(sudo_cmd(pattern="ش3$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش3$"))
+@bot.on(sudo_cmd(pattern="ش3$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    kh = await rd(icss)
+    kh = await rd(zed)
     if fy_gif3:
-        icss_caption = f"**{TSHE}**\n"
-        icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        icss_caption += f"**↫ المتـحركه الثالثه 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif3, caption=icss_caption, reply_to=kh
+        zed_caption = f"**{TSHE}**\n"
+        zed_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        zed_caption += f"**↫ المتـحركه الثالثه 𓆰.**"
+        await zed.client.send_file(
+            zed.chat_id, fy_gif3, caption=zed_caption, reply_to=kh
         )
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش4$"))
-@icssbot.on(sudo_cmd(pattern="ش4$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش4$"))
+@bot.on(sudo_cmd(pattern="ش4$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    wh = await rd(icss)
+    wh = await rd(zed)
     if fy_gif4:
         fyc = f"**{TSHE}**\n"
         fyc += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         fyc += f"**↫ المتـحركه الرابعه 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif4, caption=fyc, reply_to=wh
+        await zed.client.send_file(
+            zed.chat_id, fy_gif4, caption=fyc, reply_to=wh
         )
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش5$"))
-@icssbot.on(sudo_cmd(pattern="ش5$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش5$"))
+@bot.on(sudo_cmd(pattern="ش5$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    ih = await rd(icss)
+    ih = await rd(zed)
     if fy_gif5:
         fyc = f"**{TSHE}**\n"
         fyc += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         fyc += f"**↫ المتـحركه الخامسه 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif5, caption=fyc, reply_to=ih
+        await zed.client.send_file(
+            zed.chat_id, fy_gif5, caption=fyc, reply_to=ih
         )
 
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش6$"))
-@icssbot.on(sudo_cmd(pattern="ش6$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش6$"))
+@bot.on(sudo_cmd(pattern="ش6$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    uh = await rd(icss)
+    uh = await rd(zed)
     if fy_gif6:
         fyc = f"**{TSHE}**\n"
         fyc += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         fyc += f"**↫ المتـحركه السادسه 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif6, caption=fyc, reply_to=uh
+        await zed.client.send_file(
+            zed.chat_id, fy_gif6, caption=fyc, reply_to=uh
         )
 
 
-@icssbot.on(icss_cmd(outgoing=True, pattern="ش7$"))
-@icssbot.on(sudo_cmd(pattern="ش7$", allow_sudo=True))
-async def fygif(icss):
-    if icss.fwd_from:
+@bot.on(admin_cmd(outgoing=True, pattern="ش7$"))
+@bot.on(sudo_cmd(pattern="ش7$", allow_sudo=True))
+async def fygif(zed):
+    if zed.fwd_from:
         return
-    oh = await rd(icss)
+    oh = await rd(zed)
     if fy_gif7:
         fyc = f"**{TSHE}**\n"
         fyc += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         fyc += f"**↫ المتـحركه السابعه 𓆰.**"
-        await icss.client.send_file(
-            icss.chat_id, fy_gif7, caption=fyc, reply_to=oh
+        await zed.client.send_file(
+            zed.chat_id, fy_gif7, caption=fyc, reply_to=oh
         )
 
