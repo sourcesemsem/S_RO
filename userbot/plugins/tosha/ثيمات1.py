7225018e0ac050f988d575e076eb451ee0ea8f4c
Zedthon <84479740+Zedthon@users.noreply.github.com>
2022-01-27 20:18:45 +0300

    Create ثيمات1.py

--- userbot/plugins/tosha/ثيمات1.py
+++ userbot/plugins/tosha/ثيمات1.py
@@ -16,7 +16,7 @@
 from .. import reply_id as rd 
 from . import *
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن1$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن1$"))
 @bot.on(sudo_cmd(pattern="ن1$", allow_sudo=True))
 async def stsfanan(kim):
     if kim.fwd_from:
@@ -30,7 +30,7 @@ async def stsfanan(kim):
         await kim.client.send_file(kim.chat_id, sts_fanan, caption=kim_c, reply_to=zel)
 
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن2$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن2$"))
 @bot.on(sudo_cmd(pattern="ن2$", allow_sudo=True))
 async def stsfanan(lon):
     if lon.fwd_from:
@@ -44,7 +44,7 @@ async def stsfanan(lon):
         await lon.client.send_file(lon.chat_id, sts_fanan2, caption=ics_c, reply_to=lonid)
 
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن3$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن3$"))
 @bot.on(sudo_cmd(pattern="ن3$", allow_sudo=True))
 async def stsfanan(i):
     if i.fwd_from:
@@ -58,7 +58,7 @@ async def stsfanan(i):
         await i.client.send_file(i.chat_id, sts_fanan3, caption=tumc, reply_to=sic_id)
 
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن4$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن4$"))
 @bot.on(sudo_cmd(pattern="ن4$", allow_sudo=True))
 async def stsfanan(lon):
     if lon.fwd_from:
@@ -74,7 +74,7 @@ async def stsfanan(lon):
         )
 
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن5$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن5$"))
 @bot.on(sudo_cmd(pattern="ن5$", allow_sudo=True))
 
 async def stsfanan(tosh):
@@ -91,7 +91,7 @@ async def stsfanan(tosh):
         )
 
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن6$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن6$"))
 @bot.on(sudo_cmd(pattern="ن6$", allow_sudo=True))
 
 async def stsfanan(kimo):
@@ -108,7 +108,7 @@ async def stsfanan(kimo):
         )
 
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن7$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن7$"))
 @bot.on(sudo_cmd(pattern="ن7$", allow_sudo=True))
 async def stsfanan(ics):
     if ics.fwd_from:
@@ -124,7 +124,7 @@ async def stsfanan(ics):
         )
         
         
-@bot.on(icss_cmd(outgoing=True, pattern="ن8$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن8$"))
 @bot.on(sudo_cmd(pattern="ن8$", allow_sudo=True))
 async def stsfanan(ics):
     if ics.fwd_from:
@@ -139,7 +139,7 @@ async def stsfanan(ics):
             ics.chat_id, sts_fanan8, caption=tumc, reply_to=reply_to_id
         )
         
-@bot.on(icss_cmd(outgoing=True, pattern="ن9$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن9$"))
 @bot.on(sudo_cmd(pattern="ن9$", allow_sudo=True))
 async def stsfanan(ics):
     if ics.fwd_from:
@@ -154,7 +154,7 @@ async def stsfanan(ics):
             ics.chat_id, sts_fanan9, caption=tumc, reply_to=reply_to_id
         )
 
-@bot.on(icss_cmd(outgoing=True, pattern="ن10$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن10$"))
 @bot.on(sudo_cmd(pattern="ن10$", allow_sudo=True))
 async def stsfanan(ics):
     if ics.fwd_from:
@@ -169,7 +169,7 @@ async def stsfanan(ics):
             ics.chat_id, sts_fanan10, caption=tumc, reply_to=reply_to_id
         )
         
-@bot.on(icss_cmd(outgoing=True, pattern="ن11$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن11$"))
 @bot.on(sudo_cmd(pattern="ن11$", allow_sudo=True))
 async def stsfanan(ics):
     if ics.fwd_from:
@@ -184,7 +184,7 @@ async def stsfanan(ics):
             ics.chat_id, sts_fanan11, caption=tumc, reply_to=reply_to_id
         )
         
-@bot.on(icss_cmd(outgoing=True, pattern="ن12$"))
+@bot.on(zelzal_cmd(outgoing=True, pattern="ن12$"))
 @bot.on(sudo_cmd(pattern="ن12$", allow_sudo=True))
 async def stsfanan(ics):
     if ics.fwd_from:
