2c8e9db4fa309200b611a15523f7662899553fab
raiis1 <102451822+raiis1@users.noreply.github.com>
2022-04-23 23:17:42 +0300

    Update _an.py

--- resources/Formation/animation/_an.py
+++ resources/Formation/animation/_an.py
@@ -1,5 +1,5 @@
-#   ZEDThon - USERBOT
-#   TELE - @ZedThon
+#   RallsThon - USERBOT
+#   TELE - @RallsThon
 
 #  ======================================================= #
 
@@ -7,7 +7,7 @@ from userbot.Config import Config
 
 USERID = Config.OWNER_ID 
 ALIVE_NAME = Config.ALIVE_NAME
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Zed Userbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Ralls Userbot"
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
 #  ======================================================= #
