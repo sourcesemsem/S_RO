0a1270a31d0e985d36da043e9e30a80c5229fef3
raiis1 <102451822+raiis1@users.noreply.github.com>
2022-04-23 19:36:01 +0300

    Update متحركات.py

--- userbot/plugins/tosha/متحركات.py
+++ userbot/plugins/tosha/متحركات.py
@@ -1,5 +1,5 @@
 """
-©Zed : @ZedThon
+©Ralls : @RallsThon
   - Commends of All Gif
 """
 
@@ -7,27 +7,27 @@ from . import *
 
 @bot.on(admin_cmd(pattern="م20"))
 @bot.on(sudo_cmd(pattern="م20", allow_sudo=True))
-async def zed(tosh):
+async def Ralls(tosh):
     await eor(tosh, T)
 
 @bot.on(admin_cmd(pattern="متحركات تمبلر"))
 @bot.on(sudo_cmd(pattern="متحركات تمبلر", allow_sudo=True))
-async def zed(tosh):
+async def Ralls(tosh):
     await eor(tosh, O)
 
 @bot.on(admin_cmd(pattern="متحركات كيوت"))
 @bot.on(sudo_cmd(pattern="متحركات كيوت", allow_sudo=True))
-async def zed(tosh):
+async def Ralls(tosh):
     await eor(tosh, S)
 
 @bot.on(admin_cmd(pattern="متحركات حب"))
 @bot.on(sudo_cmd(pattern="متحركات حب", allow_sudo=True))
-async def zed(tosh):
+async def Ralls(tosh):
     await eor(tosh, H)
 
 @bot.on(admin_cmd(pattern="متحركات تحشيش"))
 @bot.on(sudo_cmd(pattern="متحركات تحشيش", allow_sudo=True))
-async def zed(tosh):
+async def Ralls(tosh):
     await eor(tosh, A)
 
 
