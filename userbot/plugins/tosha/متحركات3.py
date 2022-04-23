fe1ad8593486278e0ae9229a7b77ca9a6cdac76e
raiis1 <102451822+raiis1@users.noreply.github.com>
2022-04-23 19:43:20 +0300

    Update متحركات3.py

--- userbot/plugins/tosha/متحركات3.py
+++ userbot/plugins/tosha/متحركات3.py
@@ -1,4 +1,4 @@
-# LOVEGif for Z by: @ZedThon
+# LOVEGif for R by: @RallsThon
 
 from .. import reply_id as rd
 from . import *
@@ -6,101 +6,101 @@ from . import *
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح1$"))
 @bot.on(sudo_cmd(pattern="ح1$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    Ti = await rd(zed)
+    Ti = await rd(Ralls)
     if ve_gif:
-        zed_caption = f"**{LOVE}**\n"
-        zed_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        zed_caption += f"**↫ المتـحركه الاولى 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif, caption=zed_caption, reply_to=Ti
+        Ralls_caption = f"**{LOVE}**\n"
+        Ralls_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        Ralls_caption += f"**↫ المتـحركه الاولى 𓆰.**"
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif, caption=Ralls_caption, reply_to=Ti
         )
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح2$"))
 @bot.on(sudo_cmd(pattern="ح2$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    th = await rd(zed)
+    th = await rd(Ralls)
     if ve_gif2:
-        zed_caption = f"**{LOVE}**\n"
-        zed_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        zed_caption += f"**↫ المتـحركه الثانيه 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif2, caption=zed_caption, reply_to=th
+        Ralls_caption = f"**{LOVE}**\n"
+        Ralls_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        Ralls_caption += f"**↫ المتـحركه الثانيه 𓆰.**"
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif2, caption=Ralls_caption, reply_to=th
         )
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح3$"))
 @bot.on(sudo_cmd(pattern="ح3$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    kh = await rd(zed)
+    kh = await rd(Ralls)
     if ve_gif3:
-        zed_caption = f"**{LOVE}**\n"
-        zed_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        zed_caption += f"**↫ المتـحركه الثالثه 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif3, caption=zed_caption, reply_to=kh
+        Ralls_caption = f"**{LOVE}**\n"
+        Ralls_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        Ralls_caption += f"**↫ المتـحركه الثالثه 𓆰.**"
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif3, caption=Ralls_caption, reply_to=kh
         )
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح4$"))
 @bot.on(sudo_cmd(pattern="ح4$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    wh = await rd(zed)
+    wh = await rd(Ralls)
     if ve_gif4:
         vec = f"**{LOVE}**\n"
-        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         vec += f"**↫ المتـحركه الرابعه 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif4, caption=vec, reply_to=wh
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif4, caption=vec, reply_to=wh
         )
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح5$"))
 @bot.on(sudo_cmd(pattern="ح5$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    ih = await rd(zed)
+    ih = await rd(Ralls)
     if ve_gif5:
         vec = f"**{LOVE}**\n"
-        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         vec += f"**↫ المتـحركه الخامسه 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif5, caption=vec, reply_to=ih
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif5, caption=vec, reply_to=ih
         )
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح6$"))
 @bot.on(sudo_cmd(pattern="ح6$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    uh = await rd(zed)
+    uh = await rd(Ralls)
     if ve_gif6:
         vec = f"**{LOVE}**\n"
-        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         vec += f"**↫ المتـحركه السادسه 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif6, caption=vec, reply_to=uh
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif6, caption=vec, reply_to=uh
         )
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="ح7$"))
 @bot.on(sudo_cmd(pattern="ح7$", allow_sudo=True))
-async def vegif(zed):
-    if zed.fwd_from:
+async def vegif(Ralls):
+    if Ralls.fwd_from:
         return
-    oh = await rd(zed)
+    oh = await rd(Ralls)
     if ve_gif7:
         vec = f"**{LOVE}**\n"
-        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧℤ𝔼𝔻𝕋ℍ𝕆ℕⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        vec += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         vec += f"**↫ المتـحركه السابعه 𓆰.**"
-        await zed.client.send_file(
-            zed.chat_id, ve_gif7, caption=vec, reply_to=oh
+        await Ralls.client.send_file(
+            Ralls.chat_id, ve_gif7, caption=vec, reply_to=oh
         )
 
