4a26d8b20664f977573584d51c0755f69b7fc089
raiis1 <102451822+raiis1@users.noreply.github.com>
2022-04-23 19:27:16 +0300

    Update ثيمات.py

--- userbot/plugins/tosha/ثيمات.py
+++ userbot/plugins/tosha/ثيمات.py
@@ -1,5 +1,5 @@
 """
-©Zed : @ZedThon
+©Ralls : @RallsThon
   - Commends of All Theme
 """
 
@@ -7,12 +7,12 @@ from . import *
 
 @bot.on(admin_cmd(pattern="م30"))
 @bot.on(sudo_cmd(pattern="م30", allow_sudo=True))
-async def zed(zelzal):
-    await eor(zelzal, X)
+async def Ralls(QQ070):
+    await eor(QQ070, X)
 
 @bot.on(admin_cmd(pattern="ثيمات"))
 @bot.on(sudo_cmd(pattern="ثيمات", allow_sudo=True))
-async def zed(zelzal):
-    await eor(zelzal, ZL)
+async def Ralls(QQ070):
+    await eor(QQ070, ZL)
 
 
