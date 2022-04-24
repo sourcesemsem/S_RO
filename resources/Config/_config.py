744253f2d361e31b3a347acd08bcb5ef589dfe44
raiis1 <102451822+raiis1@users.noreply.github.com>
2022-04-24 03:59:13 +0300

    Update _config.py

--- resources/Config/_config.py
+++ resources/Config/_config.py
@@ -5,7 +5,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from ._icsconfig import Config  # @zlzzl77
+    from ._icsconfig import Config  # @QQ070
 else:
     if os.path.exists("config.py"):
         from config import Development as Config  # Hey there
