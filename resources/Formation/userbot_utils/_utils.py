c40a0df8a65a81f6ff1bbc9ab80b4e21f14ce849
روجر <104784913+rogerpq@users.noreply.github.com>
2023-03-01 19:37:29 +0300

    Update _utils.py

--- resources/Formation/userbot_utils/_utils.py
+++ resources/Formation/userbot_utils/_utils.py
@@ -253,7 +253,7 @@ def errors_handler(func):
             ftext += "\nwe respect your privacy,"
             ftext += "\nyou may not report this error if you've"
             ftext += "\nany confidential data here, no one will see your data\n\n"
-            ftext += "--------BEGIN Ralls TRACEBACK LOG--------"
+            ftext += "--------BEGIN REPTHON TRACEBACK LOG--------"
             ftext += "\nDate: " + date
             ftext += "\nGroup ID: " + str(errors.chat_id)
             ftext += "\nSender ID: " + str(errors.sender_id)
@@ -264,7 +264,7 @@ def errors_handler(func):
             ftext += "\n\nError text:\n"
             ftext += str(sys.exc_info()[1])
             new = {"error": str(sys.exc_info()[1]), "date": datetime.datetime.now()}
-            ftext += "\n\n--------END Ralls TRACEBACK LOG--------"
+            ftext += "\n\n--------END REPTHON TRACEBACK LOG--------"
 
             command = 'git log --pretty=format:"%an: %s" -5'
 
@@ -279,8 +279,8 @@ def errors_handler(func):
             from .helpers.utils import _format
 
             pastelink = _format.paste_text(ftext)
-            text = "𓆩 𝑺𝑶𝑼𝑹𝑪𝑬 𝐑𝐀𝐈𝐈𝐒 𝑬𝑹𝑹𝑶𝑹 𝑹𝑬𝑷𝑶𝑹𝑻 𓆪\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐑𝐀𝐈𝐈𝐒ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n\n"
-            link = "[مطور السـورس](https://t.me/QQ070)"
+            text = "𓆩 𝚂𝚘𝚞𝚛𝚌𝚎 𝚁𝚎𝚙𝚝𝚑𝚘𝚗 𝑬𝑹𝑹𝑶𝑹 𝑹𝑬𝑷𝑶𝑹𝑻 𓆪\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝚁𝚎𝚙𝚝𝚑𝚘𝚗ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n\n"
+            link = "[مطور السـورس](https://t.me/E_7_V)"
             text += "- إذا كنت تريد أن تتمكن من الإبلاغ عن المشكله"
             text += f"- فقط قم بتوجيه هذا الرساله الى {link}.\n"
             text += "- لا يوجد شيء مسجل باستثناء حقيقة الخطأ والتاريخ\n\n"
