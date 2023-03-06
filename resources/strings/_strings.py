4db6f54e3dd2e81aea9dd05e7443452181a953a0
روجر <104784913+rogerpq@users.noreply.github.com>
2023-03-06 14:32:07 +0300

    Update _strings.py

--- resources/strings/_strings.py
+++ resources/strings/_strings.py
@@ -3,7 +3,7 @@ from userbot.Config import Config # Ok
 HEROKU_APP_NAME = Config.HEROKU_APP_NAME or None
 HEROKU_API_KEY = Config.HEROKU_API_KEY or None
 UPSTREAM_REPO_BRANCH = Config.UPSTREAM_REPO_BRANCH
-UPSTREAM_REPO = "https://github.com/raiis1/Ralls_USERBOT"
+UPSTREAM_REPO = "https://github.com/rogerpq/Ralls_USERBOT"
 T = Config.COMMAND_HAND_LER
 
 Up_1 = "𓆰 𝗦𝗢𝗨𝗥𝗖𝗘 𝐑𝐀𝐈𝐈𝐒 - 𝑼𝑷𝑫𝑨𝑻𝑬 𝑴𝑺𝑮 𓆪\n 𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n** ⪼ يوجـد تحـديث جديد لسورس ريس ثـون ༗.**\n`{}`\n 𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n 𓆰 𝙎𝙊𝙐𝙍𝘾𝞝 𝘿𝙀𝙑 - @RallsTHON 𓆪"
