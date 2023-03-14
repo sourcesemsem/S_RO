3549764aa9be08ac5cca2fad98321e38b836181a
روجر <104784913+rogerpq@users.noreply.github.com>
2023-03-14 20:11:39 +0300

    Update _userbot.py

--- resources/Formation/userbot/_userbot.py
+++ resources/Formation/userbot/_userbot.py
@@ -139,148 +139,140 @@ elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
     Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID
 async def verifyLoggerGroup():
 
-    """
+  """
+Will verify the both loggers group
+  """
 
-    Will verify the both loggers group
+flag = False
 
-    """
+if BOTLOG:
 
-    flag = False
+     try:
 
-    if BOTLOG:
+entity = await bot.get_entity(BOTLOG_CHATID)
 
-        try:
+if not isinstance(entity, types.User) and not entity.creator:
 
-            entity = await bot.get_entity(BOTLOG_CHATID)
+if entity.default_banned_rights.send_messages:
 
-            if not isinstance(entity, types.User) and not entity.creator:
+              LOGS.info(
 
-                if entity.default_banned_rights.send_messages:
-
-                    LOGS.info(
-
-                        "᯽︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
+"᯽︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
 
                     )
 
-                if entity.default_banned_rights.invite_users:
+if entity.default_banned_rights.invite_users:
 
-                    LOGS.info(
+    LOGS.info(
 
-                        "᯽︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
+"᯽︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
 
-                    )
+                  )
 
-        except ValueError:
+ except ValueError:
 
-            LOGS.error("᯽︙تـأكد من فـار المجـموعة  PRIVATE_GROUP_BOT_API_ID.")
+LOGS.error("᯽︙تـأكد من فـار المجـموعة  PRIVATE_GROUP_BOT_API_ID.")
 
-        except TypeError:
+ except TypeError:
 
-            LOGS.error(
+ LOGS.error(
 
-                "᯽︙لا يمكـن العثور على فار المجموعه PRIVATE_GROUP_BOT_API_ID. تأكد من صحتها."
+"᯽︙لا يمكـن العثور على فار المجموعه PRIVATE_GROUP_BOT_API_ID. تأكد من صحتها."
 
-            )
+          )
 
-        except Exception as e:
+   except Exception as e:
 
-            LOGS.error(
+         LOGS.error(
 
-                "᯽︙حدث استثناء عند محاولة التحقق من PRIVATE_GROUP_BOT_API_ID.\n"
+"᯽︙حدث استثناء عند محاولة التحقق من PRIVATE_GROUP_BOT_API_ID.\n"
 
-                + str(e)
+    + str(e)
 
-            )
+          )
 
-    else:
+  else:
 
-        descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - @Repthon"
+descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - @Repthon"
 
-        photobt = await bot.upload_file(file="userbot/extras
+photobt = await bot.upload_file(file="userbot/extras/Repthon1.jpg
 
-/Repthon1.jpg")
+_, groupid = await create_supergroup(
 
-
-
-        _, groupid = await create_supergroup(
-
-            "مــجــمــوعــة أَشــعــارات ريبـــثون", bot, Config.BOT_USERNAME, descript, photobt
+"مــجــمــوعــة أَشــعــارات ريبـــثون", bot, Config.BOT_USERNAME, descript, photobt
 
         )
 
-        addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
+addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
 
-        print("᯽︙تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
+print("᯽︙تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
 
-        flag = True
+flag = True
 
-    if PM_LOGGER_GROUP_ID != -100:
+if PM_LOGGER_GROUP_ID != -100:
 
         try:
 
-            entity = await bot.get_entity(PM_LOGGER_GROUP_ID)
+entity = await bot.get_entity(PM_LOGGER_GROUP_ID)
 
-            if not isinstance(entity, types.User) and not entity.creator:
+if not isinstance(entity, types.User) and not entity.creator:
 
-                if entity.default_banned_rights.send_messages:
+if entity.default_banned_rights.send_messages:
 
-                    LOGS.info(
+        LOGS.info(
 
-                        "᯽︙الأذونات مفقودة لإرسال رسائل لـ PM_LOGGER_GROUP_ID المحدد."
+"᯽︙الأذونات مفقودة لإرسال رسائل لـ PM_LOGGER_GROUP_ID المحدد."
 
-                    )
+             )
 
-                if entity.default_banned_rights.invite_users:
+if entity.default_banned_rights.invite_users:
 
-                    LOGS.info(
+        LOGS.info(
 
-                        "᯽︙الأذونات مفقودة للمستخدمين الإضافيين لـ PM_LOGGER_GROUP_ID المحدد."
+"᯽︙الأذونات مفقودة للمستخدمين الإضافيين لـ PM_LOGGER_GROUP_ID المحدد."
 
-                    )
+              )
 
-        except ValueError:
+   except ValueError:
 
-            LOGS.error("᯽︙لا يمكن العثور على فار  PM_LOGGER_GROUP_ID. تأكد من صحتها.")
+LOGS.error("᯽︙لا يمكن العثور على فار  PM_LOGGER_GROUP_ID. تأكد من صحتها.")
 
-        except TypeError:
+except TypeError:
 
-            LOGS.error("᯽︙PM_LOGGER_GROUP_ID غير مدعوم. تأكد من صحتها.")
+LOGS.error("᯽︙PM_LOGGER_GROUP_ID غير مدعوم. تأكد من صحتها.")
 
-        except Exception as e:
+except Exception as e:
 
-            LOGS.error(
+LOGS.error(
 
-                "⌯︙حدث استثناء عند محاولة التحقق من PM_LOGGER_GROUP_ID.\n" + str(e)
-
-            )
-
-    else:
-
-        descript = "᯽︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - @Repthon"
-
-        photobt = await bot.upload_file(file="userbot/extras
-
-/Repthon2.jpg")
-
-        _, groupid = await create_supergroup(
-
-            "مجموعة التخزين", bot, Config.BOT_USERNAME, descript, photobt
+"⌯︙حدث استثناء عند محاولة التحقق من PM_LOGGER_GROUP_ID.\n" + str(e)
 
         )
 
-        addgvar("PM_LOGGER_GROUP_ID", groupid)
+else:
 
-        print("تـم عمـل الكروب التخزين بنـجاح واضافة الـفارات الـيه.")
+descript = "᯽︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - @Repthon"
 
-        flag = True
+photobt = await bot.upload_file(file="userbot/extras/Repthon1.jpg
+
+_, groupid = await create_supergroup(
+
+"مجموعة التخزين", bot, Config.BOT_USERNAME, descript, photobt
+
+      )
+
+addgvar("PM_LOGGER_GROUP_ID", groupid)
+
+print("تـم عمـل الكروب التخزين بنـجاح واضافة الـفارات الـيه.")
+
+  flag = True
 
     if flag:
 
-        executable = sys.executable.replace(" ", "\\ ")
+executable = sys.executable.replace(" ", "\\ ")
 
-        args = [executable, "-m", "userbot"]
+args = [executable, "-m", "userbot"]
 
-        os.execle(executable, *args, os.environ)
+os.execle(executable, *args, os.environ)
 
-        sys.exit(0)
+sys.exit(0)
