3a7e0d26b07fb35609f0dfa27b13e804f7e95118
روجر <104784913+rogerpq@users.noreply.github.com>
2023-03-15 15:06:30 +0300

    Update رساله.py

--- userbot/plugins/رساله.py
+++ userbot/plugins/رساله.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image, ImageDraw, ImageFont
 from . import *
+from . import ALIVE_NAME, *
 
 
 def text_set(text):
@@ -19,7 +20,7 @@ def text_set(text):
     return lines[:25]
     
 
-@Rallsthon.on(QQ070_cmd(pattern="رساله ?(.*)"))
+@bot.on(baqir_cmd(pattern="رساله ?(.*)"))
 async def writer(e):
     if e.reply_to:
         reply = await e.get_reply_message()
@@ -28,16 +29,16 @@ async def writer(e):
         text = e.text.split(maxsplit=1)[1]
     else:
         return await e.edit("**- بالـرد على نص او .رساله + النص**")
-    img = Image.open("QQ070/malath/ppho.jpg")
+    img = Image.open("ZED-BOT/malath/ppho.jpg")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("QQ070/malath/zarz.ttf", 30)
+    font = ImageFont.truetype("ZED-BOT/malath/zarz.ttf", 30)
     x, y = 150, 140
     lines = text_set(text)
     line_height = font.getsize("hg")[1]
     for line in lines:
         draw.text((x, y), line, fill=(1, 22, 55), font=font)
         y = y + line_height - 5
-    file = "QQ070.jpg"
+    file = "{ALIVE_NAME}.jpg"
     img.save(file)
     await e.reply(file=file)
     os.remove(file)
