1f78ee420eb1b7380f5fd798c587965d81b214eb
روجر <104784913+rogerpq@users.noreply.github.com>
2023-03-17 15:06:05 +0300

    Update صيد.py

--- userbot/plugins/صيد.py
+++ userbot/plugins/صيد.py
@@ -6,7 +6,7 @@ import random
 import requests
 import telethon
 from telethon.sync import functions
-from user_agent import generate_user_agent
+from telethon.tl import generate_user_agent
 
 from userbot import bot
 
