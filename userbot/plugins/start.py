ace2b2b4936c55be1980890ea676f8265471ad5c
روجر <104784913+rogerpq@users.noreply.github.com>
2023-03-22 13:38:27 +0300

    Update start.py

--- userbot/plugins/start.py
+++ userbot/plugins/start.py
@@ -8,7 +8,7 @@ from userbot_utils import sudo_cmd
 
 ownerbaqir_id = 5502537272
 @bot.on(admin_cmd(pattern="/start"))
-@bot.on(admin_cmd(pattern="/start", allow_sudo=True))
+@bot.on(sudo_cmd(pattern="/start", allow_sudo=True))
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerbaqir_id :
